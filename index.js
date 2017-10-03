3b860e3c51fd97ea3496db251d8b95cc6e2d0f3d
shahlar1 <shahlariq@code.edu.az>
2017-10-03 15:23:16 -0700

    m

---
+++ index.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+var reqem;
+
+
+reqem=prompt("reqem daxil et","")
+if(reqem==1){
+    console.log('bir')
+}
+if(reqem==2){
+    console.log('iki')
+}
+if(reqem==3){
+    console.log('uc')
+}
+if(reqem==4){
+    console.log('dord')
+}
+if(reqem==5){
+    console.log('bes')
+}
+if(reqem==6){
+    console.log('alti')
+}
+if(reqem==7){
+    console.log('yeddi')
+}
+if(reqem==8){
+    console.log('sekkiz')
+}
+if(reqem==9){
+    console.log('doqquz')
+}
+if(reqem==10){
+    console.log('on')
+}
+if(reqem==20){
+    console.log('IYIRMI')
+}
+if(reqem==30){
+    console.log('0TUZ')
+}
+if(reqem==40){
+    console.log('QIRX')
+}
+if(reqem==50){
+    console.log('ALLI')
+}
+if(reqem==60){
+    console.log('ALTMIS')
+}
+if(reqem==70){
+    console.log('YETIMIS')
+}
+if(reqem==80){
+    console.log('SEKSEN')
+}
+if(reqem==90){
+    console.log('DOGSAN')
+}
+if(reqem>99){
+    console.log('YENIDEN YAZ')
+}
+
+
+
+
+})
